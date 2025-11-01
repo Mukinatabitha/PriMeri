479ae74a519e94caec76f77f29637c5a3387dc6b
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-11-01 08:50:19 +0300

    Adding the Home Page

--- php/contact.php
+++ php/contact.php
@@ -33,7 +33,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link text-dark" href="#">Features</a>
+              <a class="nav-link text-dark" href="../html/home.html">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-dark" href="../html/catalog.html">Catalog</a>
